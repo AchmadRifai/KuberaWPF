84dadb72aeec4ee861f23406b0f121ea975e676c
AchmadRifai <achmad.rifai.jowo.asli@gmail.com>
2017-05-30 21:32:00 +0700

    Weduse kubera

---
+++ Kubera/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kubera
+{
+    public class Rupiah {
+        private long data;
+        public Rupiah(long d)
+        {
+            data = d;
+        }
+
+        public long Data { get => data; }
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
